30f98f89f9cc8cfb884d1bedd05d63e3fbba00b2
Melanie Clark <melaniefclark@gmail.com>
2024-12-20 18:11:58 +0000

    game_window.py trees added to background

--- src/game_window.py
+++ src/game_window.py
@@ -11,16 +11,26 @@ class GameWindow:
         self.display = pygame.display.set_mode((self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT))
 
         pygame.display.set_caption("Snowpult")
-        clock = pygame.time.Clock()  # ----------------
+        self.clock = pygame.time.Clock()  # Manages frame rate
 
     def draw_background(self):
         self.display.fill((135, 206, 235))  # Blue sky
         # Snow layer (x pos, y pos, width, height):
         pygame.draw.rect(self.display, (255, 255, 255), (0, 150, self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT))
 
+    def draw_trees(self):
+        for x in range(25, self.DISPLAY_WIDTH - 25, self.DISPLAY_WIDTH // 10):
+            # vertices: [x-y (starting point),.....
+            pygame.draw.polygon(self.display, (34, 139, 34),
+                                [(x, 400), (x + 25, 300),
+                                 (x + 50, 400)])  # green tree triangle - 3 points filled with colour
+            # Tree brown trunk (x pos, y pos, width, height):
+            pygame.draw.rect(self.display, (139, 69, 19), (x + 20, 400, 10, 20))
+
     # Main game loop. Keeps window open until quit
     def run(self):
         self.draw_background()
+        self.draw_trees()
 
         running = True
         while running:
@@ -28,8 +38,8 @@ class GameWindow:
                 if event.type == pygame.QUIT:
                     running = False
 
-            pygame.display.update()
-            # flip refreshes entire display surface / update - partial updates
+            pygame.display.update()  # flip refreshes entire display surface / update - partial updates for performance
+            self.clock.tick(60)  # Limit to 60 frames per second
 
         pygame.quit()
         sys.exit()
