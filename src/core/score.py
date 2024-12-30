3f363b69cc9537be52b0c174e3845c242797164d
Melanie Clark <melaniefclark@gmail.com>
2024-12-30 21:46:59 +0000

    runaway snowman added & target score image updated

--- src/core/score.py
+++ src/core/score.py
@@ -11,8 +11,8 @@ class Score:
         self.color = "red"
         self.font = pygame.font.SysFont(self.font_name, self.size)
 
-        self.green_santa_image = pygame.image.load("../assets/images/green_santa.png").convert_alpha()
-        self.green_santa = pygame.transform.scale(self.green_santa_image, (60, 60))
+        self.target_image = pygame.image.load("../assets/images/runaway_snowman.png").convert_alpha()
+        self.target_image = pygame.transform.scale(self.target_image, (50, 65))
 
     def increment_score(self):
         self.total_score += 1
@@ -20,5 +20,5 @@ class Score:
 
     def draw(self):
         score_text = self.font.render(f"Score: {self.total_score}", True, self.color)
-        self.screen.blit(self.green_santa, (700, 5))
+        self.screen.blit(self.target_image, (700, 5))
         self.screen.blit(score_text, (500, 5))
