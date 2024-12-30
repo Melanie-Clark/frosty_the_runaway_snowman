a057d218a3813c2e8a7a8f1e5fedf6ef9ba48ed2
Melanie Clark <melaniefclark@gmail.com>
2024-12-30 22:01:55 +0000

    entities updated

--- src/entitites/sprites.py
+++ src/entitites/sprites.py
@@ -1,7 +1,7 @@
 import pygame
 
 
-class SpriteSheet:
+class AnimatedSprite:
     def __init__(self, sprite_sheet, flipped=False):
         self.sprite_sheet = pygame.image.load(sprite_sheet).convert_alpha()
         self.flipped = flipped
@@ -17,10 +17,9 @@ class SpriteSheet:
 
     def sprite_animation(self, steps, width, height, scale, row_index, rotation=0):
         animation_list = []
-        animations_steps = steps  # i.e. how many sprite_sheets are idle
-        y = height * row_index  # sets y-axis to start of row number
+        y = height * row_index  # sets y-axis to row number
 
-        for x_axis in range(animations_steps):
+        for x_axis in range(steps):  # steps = number of frames
             animation_list.append(self.get_sprite(x_axis, y, width, height, scale, rotation))
         return animation_list
 
