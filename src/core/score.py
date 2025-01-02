e99378d86fc75dcc50a592f8e74f9c8219480f9f
Melanie Clark <melaniefclark@gmail.com>
2025-01-02 14:16:52 +0000

    refactored to use utils frosty methods

--- src/core/score.py
+++ src/core/score.py
@@ -1,5 +1,5 @@
-import pygame
-from src.core.global_config import FONT_NAME, COLOR
+from src.config.global_config import FEATURE_COLOR, FEATURE_FONT
+from src.utils.utils import Frosty
 
 
 class Score:
@@ -7,19 +7,15 @@ class Score:
         self.screen = screen
         self.score = score
         self.incremental_score = score
-        self.size = 40
-        self.font = pygame.font.SysFont(FONT_NAME, self.size)
-
-        self.target_image = pygame.image.load("../assets/images/runaway_snowman.png").convert_alpha()
-        self.target_image = pygame.transform.scale(self.target_image, (50, 65))
 
     def increment_score(self):
         self.incremental_score += 1
         print("Score:", self.incremental_score)
 
     def draw(self):
-        score_text = self.font.render(f"Score: {self.incremental_score}", True, COLOR)
-        self.screen.blit(self.target_image, (700, 5))
+        score_text = FEATURE_FONT.render(f"Score: {self.incremental_score}", False, FEATURE_COLOR)
+        target_image = Frosty.load_frosty(1)
+        self.screen.blit(target_image, (700, 5))
         self.screen.blit(score_text, (500, 5))
 
     def total_score(self, health):
