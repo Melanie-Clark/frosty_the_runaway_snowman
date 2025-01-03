cddeccea669e0c2e5beaa0a681a358d66df834fd
Melanie Clark <melaniefclark@gmail.com>
2025-01-03 12:15:13 +0000

    entity.py check_sprite refactored

--- src/entitites/entity.py
+++ src/entitites/entity.py
@@ -57,44 +57,40 @@ class Entity(ABC):  # inherits functionality from pygames Sprite class
         random_x_position = random.randint(0, WINDOW_WIDTH)
 
         bunny = Obstacle(bunny_sprite, random.choice(["left", "right"]), 150,
-                                 random.randint(0 - sprite_width["bunny"], WINDOW_WIDTH + sprite_width["bunny"]),
-                                 400, 500,
-                                 sprite_width["bunny"], sprite_height["bunny"],
-                                 2, 2, 4, 2, 5,
-                                 10, 55, 92, 90)
+                         random.randint(0 - sprite_width["bunny"], WINDOW_WIDTH + sprite_width["bunny"]),
+                         400, 500,
+                         sprite_width["bunny"], sprite_height["bunny"],
+                         2, 2, 4, 2, 5,
+                         10, 55, 92, 90)
 
         elf_group = [(Obstacle(elf_sprite, random.choice(["left", "right"]), 100,
                                random.randint(0 - sprite_width["elf"], WINDOW_WIDTH + sprite_width["elf"]),
                                200, 475,
-                               sprite_width["elf"],
-                               sprite_height["elf"], 2, 2, 6, 3,
-                               6, 8, 26, 46, 102))
+                               sprite_width["elf"], sprite_height["elf"],
+                               2, 2, 6, 3, 6,
+                               8, 26, 46, 102))
                      for _ in range(3)]
 
         red_santa = Obstacle(red_santa_sprite, random.choice(["left", "right"]), 75,
                              random.randint(0 - sprite_width["red_santa"], WINDOW_WIDTH + sprite_width["red_santa"]),
                              100, 425,
-                             sprite_width["red_santa"],
-                             sprite_height["red_santa"],
+                             sprite_width["red_santa"], sprite_height["red_santa"],
                              1.75, 2, 4, 3, 5,
                              25, 5, 60, 108)
 
         reindeer_group = [
             (FlyingObstacle(reindeer_sprite, "left", 200,
                             random_x_position + (i * sprite_width["reindeer"]), -20,
-                            sprite_width["reindeer"],
-                            sprite_height["reindeer"],
+                            sprite_width["reindeer"], sprite_height["reindeer"],
                             1, 2, 2, 3, 3,
                             20, 18, 100, 110))
             for i in range(4)]
 
         frosty = Target(frosty_sprite, random.choice(["left", "right"]), 150,
-                        random_x_position,
-                        200, 400,
-                        sprite_width["snowman"],
-                        sprite_height["snowman"],
-                        0.75, 1, 5, 2,
-                        2, 5, 5, 62, 85)
+                        random_x_position, 200, 400,
+                        sprite_width["snowman"], sprite_height["snowman"],
+                        0.75, 1, 5, 2, 2,
+                        5, 5, 62, 85)
 
         return [*reindeer_group, *elf_group, red_santa, bunny], frosty
 
@@ -142,16 +138,15 @@ class Obstacle(Entity):  # ------same as Target, consider overarching, then sepa
 
     def check_sprite_position(self):
         # Resets if entity moves off-screen
-        if self.x > WINDOW_WIDTH:
-            self.x = -self.sprite_width  # Resets to left edge
+        if self.x > WINDOW_WIDTH or self.x < -self.sprite_width:
             self.y = random.randint(self.min_y_range, self.max_y_range)
             self.direction = random.choice(["left", "right"])
             self.speed = random.randint(self.min_speed, self.max_speed)
-        elif self.x < -self.sprite_width:
-            self.x = WINDOW_WIDTH
-            self.y = random.randint(self.min_y_range, self.max_y_range)
-            self.direction = random.choice(["left", "right"])
-            self.speed = random.randint(self.min_speed, self.max_speed)
+
+            if self.x > WINDOW_WIDTH:
+                self.x = -self.sprite_width  # Resets to left edge
+            elif self.x < -self.sprite_width:
+                self.x = WINDOW_WIDTH
 
 
 class FlyingObstacle(Entity):
@@ -175,21 +170,19 @@ class Target(Entity):
 
     def check_sprite_position(self):
         # Resets if entity moves off-screen
-        if self.x > WINDOW_WIDTH:
-            self.x = WINDOW_WIDTH  # Resets to left edge
-            self.y = random.randint(self.min_y_range, self.max_y_range)
-            # if target goes off-screen, it returns from the same side
+        if self.x > WINDOW_WIDTH or self.x < -self.sprite_width:
             if self.direction == "left":
                 self.direction = "right"
             else:
                 self.direction = "left"
-        elif self.x < -self.sprite_width:
-            self.x = -self.sprite_width
-            self.y = random.randint(self.min_y_range, self.max_y_range)
-            if self.direction == "left":
-                self.direction = "right"
-            else:
-                self.direction = "left"
+
+            if self.x > WINDOW_WIDTH:
+                self.x = WINDOW_WIDTH  # Resets to left edge
+                self.y = random.randint(self.min_y_range, self.max_y_range)
+                # if target goes off-screen, it returns from the same side
+            elif self.x < -self.sprite_width:
+                self.x = -self.sprite_width
+                self.y = random.randint(self.min_y_range, self.max_y_range)
 
 
 if __name__ == '__main__':
