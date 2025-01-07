e8438ee1a26e7a6139f4f38bb87b68e129a35cef
Melanie Clark <melaniefclark@gmail.com>
2025-01-07 22:35:28 +0000

    general tidy_up

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,24 +1,5 @@
-import pygame
-
-from src.config.global_config import WINDOW_WIDTH, WINDOW_HEIGHT, SCREEN, TITLE_FONT, FEATURE_COLOR, TITLE_HEIGHT, \
+from src.config.global_config import WINDOW_WIDTH, SCREEN, TITLE_FONT, FEATURE_COLOR, TITLE_HEIGHT, \
     FEATURE_FONT, INSTRUCTIONS_COLOR
-
-
-# used across files such as welcome_screen and score to load and draw the main character Frosty
-class Frosty:
-    @staticmethod
-    def load_frosty(scale):
-        target_image = pygame.image.load("../assets/images/game_play_imgs/frosty.png").convert_alpha()
-        target_image = pygame.transform.scale(target_image, ((50 * scale), (65 * scale)))
-        return target_image
-
-    # draws character left and right side of welcome text
-    def draw_frosty(self, x_border):
-        target_image = self.load_frosty(4)
-        SCREEN.blit(target_image,
-                    (x_border - target_image.get_width() // 2, WINDOW_HEIGHT // 2))  # draws left-side image
-        SCREEN.blit(target_image, (WINDOW_WIDTH - x_border - target_image.get_width() // 2,
-                                   WINDOW_HEIGHT // 2))  # draws right-side image
 
 
 # class available for use across the game project
@@ -38,6 +19,5 @@ class Draw:
             y_pos += text.get_height()  # Moves position of next text to the line below
 
     # draws menu options for welcome and game over screens
-    def draw_menu_options(self):
-        menu_options = "Press P to Play or Q to Quit"
+    def draw_menu_options(self, menu_options="Press P to Play or Q to Quit"):
         self.draw_text(menu_options, FEATURE_FONT, INSTRUCTIONS_COLOR, 650)
