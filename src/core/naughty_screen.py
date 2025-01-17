9618faa9d4a7a1f6dee4e5ab75beade52517bd6e
Melanie Clark <melaniefclark@gmail.com>
2025-01-17 00:08:46 +0000

    naughty_screen.py updated

--- src/core/naughty_screen.py
+++ src/core/naughty_screen.py
@@ -1,5 +1,5 @@
 import pygame
-from src.config.global_config import FEATURE_COLOR, FEATURE_FONT, NAUGHTY_SCREEN
+from src.config.global_config import FEATURE_COLOR, NAUGHTY_SCREEN, GAME_TEXT_FONT
 from src.core.scene import Scene
 from src.utils.utils import Draw
 
@@ -13,20 +13,24 @@ class NaughtyScreen:
     # text for when health has been depleted and player is put on naughty list
     @staticmethod
     def naughty_text():
-        naughty_text = ("\nYou've run out of health!\n\n"
-                        "Santa and his friends have been hurt.\n\n"
-                        "You are now on Santa's\n"
-                        "NAUGHTY LIST!\n\n"
-                        )
-        menu_options = "Press C to Continue"
+        title = "Naughty Naughty!"
+        naughty_text = (
+            "Santa and his friends are hurt.\n\n"
+            "They can't deliver any presents.\n"
+            "Children are upset.\n\n"
+            "You've been put on the\n"
+            "NAUGHTY LIST!\n\n"
+        )
+        menu_options = "Press [RETURN] to continue"
 
-        return naughty_text, menu_options
+        return title, naughty_text, menu_options
 
     def draw_naughty_screen(self):
         print("Health depleted...player is now on the naughty list!")
         self.scene.draw_main_scene()
-        naughty_text, menu_options = self.naughty_text()
-        self.draw.draw_text(naughty_text, FEATURE_FONT, FEATURE_COLOR, 230)
+        title, naughty_text, menu_options = self.naughty_text()
+        self.draw.draw_title(title)
+        self.draw.draw_text(naughty_text, GAME_TEXT_FONT, FEATURE_COLOR, 230)
         self.draw.draw_menu_options(menu_options)
         pygame.display.update()
 
